d3056632e879b4ef5edb320928a2f7ec7dae9ef9
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2023-11-08 16:30:23 +0530

    add new user test

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,15 @@
 import { config } from "dotenv";
 config();
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+  process.env;
 
 export const Config = {
   PORT,
   NODE_ENV,
+  DB_HOST,
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
 };
--- test/user/register.spec.ts
+++ test/user/register.spec.ts
@@ -1,7 +1,25 @@
+import { DataSource } from "typeorm";
 import app from "../../src/app";
 import request from "supertest";
+import { AppDataSource } from "../../src/config/data-source";
+import { User } from "../../src/entity/User";
+import { truncateTables } from "../utils";
 
 describe("POST /auth/register", () => {
+  let connection: DataSource;
+
+  beforeAll(async () => {
+    connection = await AppDataSource.initialize();
+  });
+
+  beforeEach(async () => {
+    await truncateTables(connection);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
   describe("Given all fields", () => {
     test("should return status code 201", async () => {
       const data = {
@@ -27,7 +45,21 @@ describe("POST /auth/register", () => {
       ).toEqual(expect.stringContaining("json"));
     });
 
-    test("should persist ");
+    test("should persist user in the database", async () => {
+      // Arrange
+      const data = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "johndoe@gmail.com",
+        password: "johndoe1234",
+      };
+      // Act
+      await request(app).post("/auth/register").send(data);
+      // Assert
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      expect(users).toHaveLength(1);
+    });
   });
 
   describe("Fields are missing", () => {});
